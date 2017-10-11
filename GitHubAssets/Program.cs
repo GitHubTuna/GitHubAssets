c02ffc08c2c087a681ad831e22d5fd2f44cec78d
Fortuna Vaio <31992693+GitHubTuna@users.noreply.github.com>
2017-10-10 22:35:54 -0400

    Updated resources

--- GitHubAssets/Program.cs
+++ GitHubAssets/Program.cs
@@ -10,6 +10,11 @@ namespace GitHubAssets
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is just an empty container with links and tips on using Git.");
+
+            //Why Version Control??
+            //https://stackoverflow.com/questions/1408450/why-should-i-use-version-control
+
             //Create REPO in GitHub
             //Copy Hyperlink https://github.com/GitHubTuna/GitHubAssets.git
             //Create VS project
@@ -17,10 +22,8 @@ namespace GitHubAssets
             //Enter URL from created GitHub repo when prompted
             //Paste in where Visual Studio has "Push to Remote Repository"
 
-            Console.WriteLine("This is just an empty container with links and tips on using Git.");
-
-            //Why Version Control??
-            //https://stackoverflow.com/questions/1408450/why-should-i-use-version-control
+            //GitHub Desktop Application DOWNLOAD
+            //https://desktop.github.com/
 
             //GitHub website: GitHubTuna = my user name on GitHub
             //https://github.com/
@@ -39,6 +42,41 @@ namespace GitHubAssets
             //Rebasing a Branch
             //https://git-scm.com/docs/git-rebase
             //https://git-scm.com/book/en/v2/Git-Branching-Rebasing
+
+            //****************************************************************************
+            //HELPERS BELOW NOT RELATED TO GIT
+            //****************************************************************************
+
+            //Lansing Codes SLACK
+            //https://lansingcodes.slack.com/messages/C064C9H7A
+            //#dotnet = channel for us to place information on our Glug.Net MeetUp
+
+            //LAUNCHY
+            //Application Launcher and much more
+            //ALT + SPACE to ACTIVATE LAUNCHY
+            //Same key combo to close
+            //http://www.launchy.net/
+            //You can open the WEB
+            //Type C:\ to open folders on your PC
+            //Mouse to edit mouse settings
+            //Folder Options to show hidden files 
+
+            //METRO STUDIO
+            //Looking for icons for your application
+            //XAML representation of paths to create images available 
+            //https://www.syncfusion.com/downloads/metrostudio
+
+            //C# Design Patterns 
+            //http://www.dofactory.com/net/design-patterns
+            //https://www.lynda.com/C-tutorials/C-Design-Patterns/473890-2.html
+            //https://www.youtube.com/watch?v=YDobmucohqk
+
+            //Create software tests
+            //https://hackernoon.com/common-excuses-why-developers-dont-test-their-software-908a465e122c
+
+            //****************************************************************************
+            //END HELPERS
+            //****************************************************************************
 
         }
     }
