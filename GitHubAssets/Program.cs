1d3be562a717d6e1885b74f22dfb51d536f12765
Fortuna Vaio <31992693+GitHubTuna@users.noreply.github.com>
2017-10-10 20:42:29 -0400

    Updaed help on GitHub setup

--- GitHubAssets/Program.cs
+++ GitHubAssets/Program.cs
@@ -11,6 +11,9 @@ namespace GitHubAssets
         static void Main(string[] args)
         {
             //Repo for new project with Git will not exist until your first commit to the MASTER in GitHub
+            //Create REPO in GitHub
+            //Copy Hyperlink https://github.com/GitHubTuna/GitHubAssets.git
+            //Paste in where it says "Push to Remote Repository"
         }
     }
 }
