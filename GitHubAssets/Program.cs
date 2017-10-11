2975776ce6a2724388378a362d8e612c6eb46ae1
Fortuna Vaio <31992693+GitHubTuna@users.noreply.github.com>
2017-10-10 20:57:07 -0400

    Added web links for help with Git

--- GitHubAssets/Program.cs
+++ GitHubAssets/Program.cs
@@ -14,6 +14,30 @@ namespace GitHubAssets
             //Create REPO in GitHub
             //Copy Hyperlink https://github.com/GitHubTuna/GitHubAssets.git
             //Paste in where it says "Push to Remote Repository"
+
+            Console.WriteLine("This is just an empty container with links and tips on using Git.");
+
+            //Why Version Control??
+            //https://stackoverflow.com/questions/1408450/why-should-i-use-version-control
+
+            //GitHub website: GitHubTuna = my user name on GitHub
+            //https://github.com/
+            //You can search GitHub for my project names to view a read only version of my projects
+
+            //Git for Beginners:
+            //https://www.webpagefx.com/blog/web-design/git-tutorials-beginners/
+
+            //Git Best Practices
+            //https://sethrobertson.github.io/GitBestPractices/
+
+            //Git Bash:
+            //http://gitready.com/
+            //https://education.github.com/git-cheat-sheet-education.pdf
+
+            //Rebasing a Branch
+            //https://git-scm.com/docs/git-rebase
+            //https://git-scm.com/book/en/v2/Git-Branching-Rebasing
+
         }
     }
 }
